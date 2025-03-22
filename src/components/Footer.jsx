f10e25f267364372a91ef80bba7ac1979b382368
omor faruk <md3147693@gmail.com>
2025-03-22 21:46:16 +0600

    merge

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0 w-full flex justify-around p-5 text-xl md:text-2xl text-white bg-green-500 p-6 font-bold">
+      <h1 className="">
+        Complete Task : {0}
+      </h1>
+      <h1 className="">
+        Total Task : {0}
+      </h1>
+    </div>
+  );
+}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ export default function TodoItems({ todos ,removeTodo}) {
   return todos.map((todo ,index) => {
     return (
         <div
-        key={todo}
+        key={index}
         className="flex items-center justify-center relative w-2/3 mx-auto bg-gray-200 mt-5 p-5 rounded-md"
       >
         <h1 className="font-bold text-xl md:text-2xl lg:text-3xl text-center flex-grow">
